d639b4f116f3ad07611b24de549dad2d3422abe0
camila.barbosa <camila.barbosa@totemti.com.br>
2024-05-02 21:20:19 -0400

    feat: implementação conta terminal

---
+++ conta-banco/src/ContaBanco.java
@@ -0,0 +1,60 @@
+public class ContaBanco {
+  
+  private int numero;
+  private String agencia;
+  private String nomeCliente;
+  private double saldo;
+  
+  public ContaBanco() {
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public String getAgencia() {
+    return agencia;
+  }
+
+  public void setAgencia(String agencia) {
+    this.agencia = agencia;
+  }
+
+  public String getNomeCliente() {
+    return nomeCliente;
+  }
+
+  public void setNomeCliente(String nomeCliente) {
+    this.nomeCliente = nomeCliente;
+  }
+
+  public double getSaldo() {
+    return saldo;
+  }
+
+  public void setSaldo(double saldo) {
+    this.saldo = saldo;
+  }
+
+  public double depositar(double valor) {
+    this.saldo += valor;
+    return this.saldo;
+  }
+
+  public boolean sacar(double valor) {
+    if (valor > this.saldo) {
+      return false;
+    }
+    this.saldo -= valor;
+    return true;
+  }
+
+  public double consultarSaldo() {
+    return this.saldo;
+  }
+
+}
--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ContaBanco conta = new ContaBanco();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(' ');
+        System.out.println("Bem vindo ao nosso sistema de Conta Terminal! Vamos criar sua conta. Por favor informe os dados a seguir.");
+        System.out.println("Número Agência com dígito:");
+        conta.setAgencia(scanner.next());
+        System.out.println("Número da conta corrente:");
+        conta.setNumero(scanner.nextInt());
+        scanner.nextLine();
+        System.out.println("Nome do titular da conta:");
+        conta.setNomeCliente(scanner.nextLine());
+        System.out.println("Saldo inicial:");
+        conta.setSaldo(scanner.nextDouble());
+        String mensagemBoasVindas = String.format("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo de %f já está disponível para saque.",
+            conta.getNomeCliente(), conta.getAgencia(), conta.getNumero(), conta.consultarSaldo());
+        System.out.println(mensagemBoasVindas);
+        System.out.println("Informe o valor que deseja sacar:");
+        boolean saqueOk = conta.sacar(scanner.nextDouble());
+        if (saqueOk) {
+            System.out.println("Seu saque foi realizado com sucesso!");
+            System.out.println("O saldo disponível atual é:");
+            System.out.println(conta.consultarSaldo());
+        } else {
+            System.out.println("Seu saldo é insuficiente!");
+            System.out.println("O saldo disponível atual é:");
+            System.out.println(conta.consultarSaldo());
+        }
     }
 }
